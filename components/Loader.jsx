f38b83e44e4fffe7bbfda52fe0838b6449adfea8
ADeslarzes <aymeric.deslarzes@gmail.com>
2024-07-22 15:46:15 +0200

    [Update] on the loader

--- components/Loader.jsx
+++ components/Loader.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Loader() {
   return (
     <div className="w-screen h-screen absolute inset-0 flex justify-center items-center z-50 bg-black">
-      <div className="w-[200px] animate-spin">
+      <div className="w-[200px] animate-pulse">
         <img src="/ssa_logo/SSA_LOGO_FULL.png" alt="SSA_Logo" />
       </div>
     </div>
