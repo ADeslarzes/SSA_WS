ba02676b4cd2a2b77db9278d4fbccbbe78e295cb
ADeslarzes <aymeric.deslarzes@gmail.com>
2024-09-24 18:03:32 +0200

    [Update] Time line

--- pages/studio.js
+++ pages/studio.js
@@ -114,15 +114,15 @@ export default function studionew() {
             </div>
           </div>
         {/* TimeLIne */}
-          <div className="mt-12 flex flex-col lg:flex-row w-full p-8 md:px-28 lg:max-w-[2000px] justify-between gap-x-[120px] mx-auto">
-            <div className="text-[#B22222] text-3xl md:text-5xl w-[150px] mb-8 lg:mb-10 h-full md:sticky top-12 font-Lato">
+          <div className="mt-12 flex flex-col md:flex-row w-full p-8 md:px-28 lg:max-w-[2000px] justify-between gap-x-[120px] mx-auto">
+            <div className="text-[#B22222] text-3xl md:text-5xl w-[150px] mb-8 md:mb-0 h-full md:sticky top-12 font-Lato">
               Timeline
-            </div>
-            <div className="flex-grow">
+              <div className="flex-grow">
               <div className="flex flex-col max-w-[1500px] bg-black">
                 {/* An horizontal time line that we can scroll and expend the evenment we want*/} 
                 <TimeLine/>
               </div>
+            </div>
             </div>
           </div>
         {/* Main projects */}
